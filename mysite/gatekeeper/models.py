410f3d89055172634d1c183d702db49d287dd7b0
edely <edely.gomes@gmail.com>
2017-09-24 19:53:53 -0300

    creating models / configuring database

--- mysite/gatekeeper/models.py
+++ mysite/gatekeeper/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Sugestion(models.Model):
+	title = models.CharField(max_length=80)
+	description = models.TextField()
+	pub_date = models.DateTimeField('date published')
+	#author
+	#thumbsup
+	#thumbsdown
+	#location
+	#tags
