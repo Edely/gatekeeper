f75f777ba753f6be321c267c0cba3335f1627525
edely <edely.gomes@gmail.com>
2017-09-26 08:48:25 -0300

    creating users

--- mysite/gatekeeper/models.py
+++ mysite/gatekeeper/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Sugestion(models.Model):
 	"""
@@ -15,3 +16,25 @@ class Sugestion(models.Model):
 
 	def __str__(self):
 		return self.title
+
+class Journalist(User):
+	"""
+	People who can consume the sugestions of pitch.
+	"""
+	#herited from User:
+	#username
+	#password
+	#email
+	#first_name
+	#last_name
+	
+	company = models.CharField(max_length=40)
+	
+	def __str__(self):
+		return self.username
+	
+class Citizen(User):
+	"""
+	People whom make sugestions of pitch.
+	"""	
+	neighbourhood = models.CharField(max_length=80)	
